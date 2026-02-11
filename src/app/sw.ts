c31839cf40bf2016f45c27af407cb9ee535fb620
yanlima <yangclima@gmail.com>
2026-02-11 11:50:51 -0300

    fix: remove `Serwist` logic from `sw.ts`

--- src/app/sw.ts
+++ src/app/sw.ts
@@ -1,27 +1,3 @@
 /// <reference lib="webworker" />
 
-import { Serwist } from 'serwist';
-
-declare const self: ServiceWorkerGlobalScope & {
-  __SW_MANIFEST: Array<{
-    url: string;
-    revision?: string | null;
-  }>;
-};
-
-const OFFLINE_URL = '/dashboard/individual';
-
-const serwist = new Serwist({
-  precacheEntries: [
-    ...self.__SW_MANIFEST,
-    { url: OFFLINE_URL, revision: null }
-  ],
-  skipWaiting: true,
-  clientsClaim: true,
-
-  importScripts: ['/firebase-messaging-sw.js']
-});
-
-serwist.addEventListeners();
-
-serwist.addEventListeners();
+importScripts('/firebase-messaging-sw.js');
